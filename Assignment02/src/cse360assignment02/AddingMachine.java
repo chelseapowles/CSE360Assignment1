5d093b4b2a2b14cd2e652f8e8292c8bdd44594e7
chelseapowles <cpowles@asu.edu>
2020-10-05 12:40:55 -0700

    Initial commit of adding machine.

---
+++ Assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,53 @@
+
+//package name
+package cse360assignment02;
+
+// AddingMachine class
+public class AddingMachine 
+{
+	// initialize private variable
+	private int total;
+	
+	
+	// constructor
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}//end of constructor
+	
+	
+	// get method for total
+	public int getTotal () 
+	{
+		return 0;
+	}//end of getter
+	
+	
+	// add method that takes in an integer
+	public void add (int value) 
+	{
+	  
+	}//end of add
+	
+	
+	// subtract method that takes in an integer
+	public void subtract (int value) 
+	{
+	  
+	}//end of subtract
+	
+	
+	//to string method
+	public String toString () 
+	{
+		return "";
+	}//end of to string
+	
+	
+	// method to clear
+	public void clear() 
+	{
+	  
+	}//end of clear
+	
+}//end of public class
