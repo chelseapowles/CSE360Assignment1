54335217a474a90f224b205b4197b7f42bccb2be
chelseapowles <cpowles@asu.edu>
2020-10-05 17:43:02 -0700

    The changes to the adding machine with comments.

--- Assignment02/src/cse360assignment02/AddingMachine.java
+++ Assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,52 +1,70 @@
+//Chelsea Powles || Assignment 1 || CSE 360 Tuesday
+
 
 //package name
 package cse360assignment02;
 
+
+import java.util.*;
+import java.io.*;
+
 // AddingMachine class
 public class AddingMachine 
 {
+	
 	// initialize private variable
 	private int total;
+	//initialize a private string buffer to concatenate the string
+	private StringBuffer history;
 	
 	
 	// constructor
-	public AddingMachine () 
+	public AddingMachine() 
 	{
 		total = 0;  // not needed - included for clarity
+		// initialize the string buffer to 0 because thats what history starts at
+		history = new StringBuffer("0");
 	}//end of constructor
 	
 	
 	// get method for total
-	public int getTotal () 
+	public int getTotal() 
 	{
-		return 0;
+		//The getTotal method should return the current total
+		return total;
 	}//end of getter
 	
 	
 	// add method that takes in an integer
-	public void add (int value) 
+	public void add(int value) 
 	{
-	  
+		//The add method should add the parameter to the total variable
+		total = total + value;
+		history.append(" + " + value);
 	}//end of add
 	
 	
 	// subtract method that takes in an integer
-	public void subtract (int value) 
+	public void subtract(int value) 
 	{
-	  
+		//The subtract method should subtract the parameter from the total variable
+		total = total - value;
+		history.append(" - " + value);
 	}//end of subtract
 	
 	
 	//to string method
-	public String toString () 
+	public String toString() 
 	{
-		return "";
+		return history.toString();
 	}//end of to string
 	
 	
-	// method to clear
+	// method to clear the history
 	public void clear() 
 	{
+		//The clear method should clear your "memory".
+		history.delete(0, history.length());
 	  
 	}//end of clear
 	
